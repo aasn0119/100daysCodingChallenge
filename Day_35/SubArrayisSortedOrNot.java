c360829eedecbf084af5be1524aa5c4c1c57316c
aasn0119 <abhitrivedi119@gmail.com>
2023-11-06 20:30:43 +0530

    Day_35 Problems and Their Solutions

---
+++ Day_35/SubArrayisSortedOrNot.java
@@ -0,0 +1,53 @@
+package Day_35;
+
+public class SubArrayisSortedOrNot {
+
+    static void reverse(int[] a, int x, int y) {
+        while (x < y) {
+            int temp = a[x];
+            a[x] = a[y];
+            a[y] = temp;
+            x++;
+            y--;
+        }
+    }
+
+    static boolean sortArr(int[] a, int n) {
+        int x = -1;
+        int y = -1;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                if (x == -1) {
+                    x = i;
+                }
+                y = i + 1;
+            }
+        }
+
+        if (x != -1) {
+            reverse(a, x, y);
+            for (int i = 0; i < n - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 5, 4, 3 };
+        int n = arr.length;
+
+        if (sortArr(arr, n)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+
+// This code is contributed by aditya942003patil
