b156a55cceddaae84de815243b0663c22a9162bb
aasn0119 <abhitrivedi119@gmail.com>
2023-12-04 21:08:35 +0530

    Day_63 Problems and Their Solutions

---
+++ Day_63/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,37 @@
+package Day_63;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class MaximumDepthOfBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.left.left = new TreeNode(15);
+        root.left.right = new TreeNode(7);
+
+        int maxDepth = maxDepth(root);
+        System.out.println("Maximum depth of the binary tree: " + maxDepth);
+    }
+}
---
+++ Day_63/MergeBinaryTrees.java
@@ -0,0 +1,58 @@
+package Day_63;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class MergeBinaryTrees {
+
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+        if (root2 == null) {
+            return root1;
+        }
+
+        TreeNode newNode = new TreeNode(root1.val + root2.val);
+        newNode.left = mergeTrees(root1.left, root2.left);
+        newNode.right = mergeTrees(root1.right, root2.right);
+
+        return newNode;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(3);
+        root1.right = new TreeNode(2);
+        root1.left.left = new TreeNode(5);
+
+        TreeNode root2 = new TreeNode(2);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(3);
+        root2.left.left = new TreeNode(4);
+        root2.right.right = new TreeNode(7);
+
+        TreeNode mergedTree = mergeTrees(root1, root2);
+        System.out.println("Merged tree:");
+        printTree(mergedTree);
+    }
+
+    private static void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.val + " ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+}
